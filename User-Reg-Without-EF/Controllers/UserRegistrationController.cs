fa17cbe04fdd4ccf3c3a96019c81f9d36f2eb828
KwangSoo Yoo <kwangsoo.yoo@hotmail.com>
2020-10-28 19:01:15 +0900

    Index 기능 추가

--- User-Reg-Without-EF/Controllers/UserRegistrationController.cs
+++ User-Reg-Without-EF/Controllers/UserRegistrationController.cs
@@ -10,8 +10,30 @@ using User_Reg_Without_EF.Models;
 
 namespace User_Reg_Without_EF.Controllers {
     public class UserRegistrationController : Controller {
+        
+        private string connection = "Data Source=dbserver;Initial Catalog=Employee;User ID=sa;Password=!7mlgtaw3";
+
         public IActionResult Index() {
-            return View();
+            List<UserRegClass> userRegClass = new List<UserRegClass>();
+            using (SqlConnection sqlconn = new SqlConnection(connection)) {
+                string sqlquery = $"Select * from NewEmployee";
+                using (SqlCommand sqlcomm = new SqlCommand(sqlquery, sqlconn)) {
+                    sqlconn.Open();
+                    SqlDataReader rdr = sqlcomm.ExecuteReader();
+                    while (rdr.Read()) {
+                        UserRegClass uRC = new UserRegClass {
+                            UserId = Convert.ToInt32(rdr["UserId"]),
+                            Username = rdr["Username"].ToString(),
+                            Uemail = rdr["Uemail"].ToString(),
+                            Pwd = rdr["Pwd"].ToString(),
+                            MartialStatus = rdr["MartialStatus"].ToString(),
+                            Gender = Convert.ToChar(rdr["Gender"])
+                        };
+                        userRegClass.Add(uRC);
+                    }
+                }
+            }
+            return View(userRegClass);
         }
 
 
@@ -24,8 +46,6 @@ namespace User_Reg_Without_EF.Controllers {
 
         [HttpPost]
         public IActionResult Create(UserRegClass ur) {
-            string connection = "Data Source=dbserver;Initial Catalog=Employee;User ID=sa;Password=!7mlgtaw3";
-
             using (SqlConnection sqlconn = new SqlConnection(connection)) {
                 string sqlquery = $"insert into NewEmployee (Username, Uemail, Pwd, MartialStatus, Gender) values ('{ur.Username}', '{ur.Uemail}', '{ur.Pwd}', '{ur.MartialStatus}', '{ur.Gender}')";
                 using (SqlCommand sqlcomm = new SqlCommand(sqlquery,sqlconn)) {
