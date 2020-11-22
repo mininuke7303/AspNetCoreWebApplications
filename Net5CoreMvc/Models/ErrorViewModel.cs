c1fe3d83347e0e16ffcd4221af6b9963af2a1888
KwangSoo Yoo <kwangsoo.yoo@hotmail.com>
2020-11-22 22:25:05 +0900

    Solutions Merge

---
+++ Net5CoreMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Net5CoreMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
